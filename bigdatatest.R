25e1ab2e02dbe721db3b706a9b7e2f80c0c25f7e
Mark M. Fredrickson <mark.m.fredrickson@gmail.com>
2013-08-09 00:00:00 +0000

    version 0.9-0

--- bigdatatest.R
+++
@@ -1,9 +0,0 @@
-set.seed(20130424)
-k <- 10000 # number of units in each of the treated and conrol groups
-
-distances <- matrix(abs(rnorm(k * k)), nrow = k, ncol = k)
-
-rownames(distances) <- paste("T", 1:k, sep = "")
-colnames(distances) <- paste("C", 1:k, sep = "")
-
-fullmatch(distances)
